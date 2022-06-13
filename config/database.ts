656bc300414a973fb41eda1b9f08e5bd2afed3b7
Robert <treboryx@gmail.com>
2022-06-13 16:25:38 +0300

    rename vars

--- config/database.ts
+++ config/database.ts
@@ -36,11 +36,11 @@ const databaseConfig: DatabaseConfig = {
     pg: {
       client: 'pg',
       connection: {
-        host: Env.get('PG_HOST'),
-        port: Env.get('PG_PORT'),
-        user: Env.get('PG_USER'),
-        password: Env.get('PG_PASSWORD', ''),
-        database: Env.get('PG_DB_NAME'),
+        host: Env.get('PGHOST'),
+        port: Env.get('PGPORT'),
+        user: Env.get('PGUSER'),
+        password: Env.get('PGPASSWORD', ''),
+        database: Env.get('PGDATABASE'),
       },
       migrations: {
         naturalSort: true,
